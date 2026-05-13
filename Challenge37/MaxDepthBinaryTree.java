bd7d7007c39fb86f04daa8f637b16d5562b4eb05
HenryMTakemoto <henry.takemoto@gmail.com>
2026-05-13 10:58:55 -0300

    Solving max depth in a binary tree

---
+++ Challenge37/MaxDepthBinaryTree.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+    public int maxDepth(TreeNode root) {
+        int depth = 0; 
+        
+        if (root == null) return depth;
+
+        int depthLeft = maxDepth(root.left);
+        int depthRight = maxDepth(root.right);
+
+        depth++;
+        if(depthRight >= depthLeft) return depthRight+depth;
+        else return depthLeft+depth; 
+
+    }
+}
